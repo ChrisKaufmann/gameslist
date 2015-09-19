12b8d60542611d7b5b9beedb6967bc0a80426ddc
Chris K <ckaufmann@connamara.com>
2015-09-19 15:03:34 -0500

    More layouts

--- static/gameslist.js
+++ static/gameslist.js
@@ -1,10 +1,11 @@
 function show(thingtoshow,filter)
 {
+console.log("show("+thingtoshow+","+filter+")")
 	$.ajax({
 		type: "GET",
 		url: '/list/'+thingtoshow+'?filter='+filter, 
 		success:function(html){
-			if(thingtoshow != 'collection') {
+/*			if(thingtoshow != 'collection') {
 				$.ajax({
 					type:"GET",
 					url:'/toggle/'+thingtoshow,
@@ -18,17 +19,20 @@ function show(thingtoshow,filter)
 			} else {
 				$('#toggle_container').html('');
 			}
+*/
 			$('#content_div').html(html);
 			$('#header-collection').css("text-decoration", "none")
 			$('#header-consoles').css("text-decoration", "none")
 			$('#header-games').css("text-decoration", "none")
 			$('#header-'+thingtoshow).css("text-decoration", "underline")
-            $('#secondary_div').html("")
+    $('#secondary_div').html("")
 			}
 		})
 }
 function show_games(console_id)
 {
+	console.log("show_games("+console_id+")")
+	$('#secondary_div').html('Loading...');
 	$.ajax({
 		type: "GET", url: '/list/games?filter=console&console_id='+console_id,
 		success:function(html){
@@ -39,19 +43,20 @@ function show_games(console_id)
 }
 function add_console(form)
 {
-        $('menu_status').innerHTML='Adding...';
-        var newcon =form.add_console_text.value;
-        $.ajax({type: "GET",url: '/console/new/'+newcon,success:function(html)
-        {
-                $('#menu_status').html(html);
-                form.add_console_text.value="";
-        }})
+	console.log("add_console("+form+")")
+	$('menu_status').innerHTML='Adding...';
+	var newcon =form.add_console_text.value;
+	$.ajax({type: "GET",url: '/console/new/'+newcon,success:function(html){
+		$('#menu_status').html(html);
+		form.add_console_text.value="";
+	}})
 }
 function add_game(form)
 {
 	var newgame = form.add_game_text.value;
 	var index=form.add_game_select.selectedIndex;
 	var selvalue=form.add_game_select.options[index].value;
+	console.log("add_game("+form+")")
 	var data="console_id="+selvalue+"&game_name="+newgame
 	$.ajax({type: "POST", url: '/console/newgame', data:data, success:function(html){
 		$('#menu_status').html(html);
@@ -60,6 +65,7 @@ function add_game(form)
 }
 function toggle_owned(id)
 {
+	console.log("toggle_owned("+id+")")
 	var data="action=toggle";
 	$.ajax({type: "POST", url: '/thing/'+id, data:data, success:function(html)
 	{
